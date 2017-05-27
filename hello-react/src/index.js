f4d7a0ddb4e0516994aed9b3644125c5f621fc93
kmsheng <kmsh3ng@gmail.com>
2017-05-27 21:05:05 +0800

    add Header class

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">React 小書</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
 registerServiceWorker();
