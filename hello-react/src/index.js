e6c8cffba34ef292acab49a3da525b7ab4f026d5
kmsheng <kmsh3ng@gmail.com>
2017-05-28 23:33:53 +0800

    add stuff for lesson12

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -3,38 +3,16 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-class LikeButton extends Component {
-
-  static defaultProps = {
-    likeText: '點讚',
-    cancelText: '取消'
-  };
-
-  constructor() {
-    super();
-    this.state = {isLiked: false};
-  }
-
-  handleClickOnLikeButton = () => {
-    this.setState((prevState) => ({isLiked: ! prevState.isLiked}));
-  };
-
-  render () {
-    const {isLiked} = this.state;
-    const {likeText, cancelText} = this.props;
-    return (
-      <button onClick={this.handleClickOnLikeButton}>
-        {isLiked ? cancelText : likeText} 👍
-      </button>
-    )
-  }
-}
+const HelloWorld = (props) => {
+  const sayHi = (event) => console.log('hello world');
+  return <div onClick={sayHi}>Hello World</div>;
+};
 
 class Index extends Component {
   render () {
     return (
       <div>
-        <LikeButton likeText="like" cancelText="cacnel" />
+        <HelloWorld />
       </div>
     )
   }
