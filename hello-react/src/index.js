3ba80c37596b86b72bad5cd7d123605c80391d80
kmsheng <kmsh3ng@gmail.com>
2017-05-28 23:54:33 +0800

    add stuff for lesson14

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,32 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
-
-const users = [
-  {username: 'Jerry', age: 21, gender: 'male'},
-  {username: 'Tomy', age: 22, gender: 'male'},
-  {username: 'Lily', age: 19, gender: 'female'},
-  {username: 'Lucy', age: 20, gender: 'female'}
-]
-
-const User = (props) => {
-  const {user} = props;
-  return (
-    <div>
-      <div>姓名：{user.username}</div>
-      <div>年龄：{user.age}</div>
-      <div>性别：{user.gender}</div>
-      <hr />
-    </div>
-  );
-};
+import CommentApp from './CommentApp';
 
 class Index extends Component {
   render () {
     return (
       <div>
-        {users.map((user) => <User key={`user-row-${user.username}`} user={user} />)}
+        <CommentApp />
       </div>
     )
   }
