0d2950e261aab6caef01ffeca8c9fc65c2a50895
kmsheng <kmsh3ng@gmail.com>
2017-05-30 10:17:15 +0800

    remove un-used components

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -5,72 +5,10 @@ import PropTypes from 'prop-types';
 
 import './index.css';
 
-class Input extends Component {
-
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-    onChange: PropTypes.func.isRequired
-  };
-
-  static defaultProps = {
-    onChange: () => {}
-  };
-
-  render () {
-    const {onChange, value} = this.props;
-    return (
-      <div>
-        <input type="number" onChange={onChange} value={value} min={0} max={1} step={0.1} />
-      </div>
-    )
-  }
-}
-
-class PercentageShower extends Component {
-
-  constructor() {
-    super();
-    this.state = {inputValue: ''};
-  }
-
-  handleInputChange = (event) => {
-    this.setState({inputValue: event.target.value});
-  };
-
-  getPercent() {
-    const inputValue = parseFloat(this.state.inputValue);
-    if (isNaN(inputValue)) {
-      return '0%';
-    }
-    return `${(inputValue * 100).toFixed(2)}%`;
-  }
-
-  render () {
-    const {inputValue} = this.state;
-    return (
-      <div>
-        <Input value={inputValue} onChange={this.handleInputChange} />
-        <span>{this.getPercent()}</span>
-      </div>
-    )
-  }
-}
-
-class PercentageApp extends Component {
-  render () {
-    return (
-      <div>
-        <PercentageShower />
-      </div>
-    )
-  }
-}
-
 class Index extends Component {
   render () {
     return (
       <div>
-        <PercentageApp />
       </div>
     )
   }
