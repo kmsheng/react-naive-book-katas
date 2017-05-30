4bbb8866e78600f27ebc812a97df79a04ca1bee8
kmsheng <kmsh3ng@gmail.com>
2017-05-30 10:18:31 +0800

    remove PropTypes

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import PropTypes from 'prop-types';
 
 import './index.css';
 
