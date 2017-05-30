14db328160d8a95275315aece8e21731e3fb1557
kmsheng <kmsh3ng@gmail.com>
2017-05-30 10:18:39 +0800

    add demo class Header

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,10 +4,36 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
+class Header extends Component {
+
+  constructor () {
+    super();
+    console.log('construct');
+  }
+
+  componentWillMount () {
+    console.log('component will mount');
+  }
+
+  componentDidMount () {
+    console.log('component did mount');
+  }
+
+  render () {
+    console.log('render');
+    return (
+      <div>
+        <h1 className="title">React 小书</h1>
+      </div>
+    )
+  }
+}
+
 class Index extends Component {
   render () {
     return (
       <div>
+        <Header />
       </div>
     )
   }
