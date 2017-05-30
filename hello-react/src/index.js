19fe778bf74bc5265e5ad8e63d04901829ff0222
kmsheng <kmsh3ng@gmail.com>
2017-05-30 11:09:27 +0800

    remove un-used stuff

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,32 +4,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
-class AutoFocusInput extends Component {
-
-  componentDidMount() {
-    this.input.focus();
-  }
-
-  focus() {
-    this.input.focus();
-  }
-
-  render() {
-    return <input ref={(input) => this.input = input} />
-  }
-}
-
 class Index extends Component {
 
-  handleButtonClick = () => this.refs.autoFocusInput.focus();
-
-  render () {
+  render() {
     return (
       <div>
-        <AutoFocusInput ref="autoFocusInput" />
-        <button onClick={this.handleButtonClick}>focus !</button>
       </div>
-    )
+    );
   }
 }
 
