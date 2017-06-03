4127f547c8c4bea07dab148a391728a4df2b4fdc
kmsheng <kmsh3ng@gmail.com>
2017-06-03 10:46:51 +0800

    add demo classes

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -5,11 +5,53 @@ import PropTypes from 'prop-types';
 
 import './index.css';
 
+class Header extends Component {
+  render () {
+    return (
+    <div>
+      <h2>This is header</h2>
+      <Title />
+    </div>
+    )
+  }
+}
+
+class Main extends Component {
+  render () {
+    return (
+    <div>
+      <h2>This is main</h2>
+      <Content />
+    </div>
+    )
+  }
+}
+
+class Title extends Component {
+  render () {
+    return (
+      <h1>React.js 小书标题</h1>
+    )
+  }
+}
+
+class Content extends Component {
+  render () {
+    return (
+    <div>
+      <h2>React.js 小书内容</h2>
+    </div>
+    )
+  }
+}
+
 class Index extends Component {
 
   render () {
     return (
       <div>
+        <Header />
+        <Main />
       </div>
     )
   }
