ef1eed2c0107d55212b649a1b7280f72a9f4d386
kmsheng <kmsh3ng@gmail.com>
2017-06-03 10:41:01 +0800

    remove un-used files

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -4,42 +4,12 @@ import registerServiceWorker from './registerServiceWorker';
 import PropTypes from 'prop-types';
 
 import './index.css';
-import CommentApp from './CommentApp';
 
 class Index extends Component {
-
-  static childContextTypes = {
-    now: PropTypes.number
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {now: +new Date()};
-    this.timer = null;
-  }
-
-  getChildContext() {
-    return {
-      now: this.state.now
-    };
-  }
-
-  updateTime = () => {
-    this.setState({now: +new Date()});
-  };
-
-  componentDidMount() {
-    this.timer = setInterval(this.updateTime, 5000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
 
   render () {
     return (
       <div>
-        <CommentApp />
       </div>
     )
   }
