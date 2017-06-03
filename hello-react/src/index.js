6513d69cf508ef14dd976754bdc36caf1403f26c
kmsheng <kmsh3ng@gmail.com>
2017-06-03 11:09:54 +0800

    add dispatch function

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -26,4 +26,20 @@ function renderContent(content) {
   contentDOM.style.color = content.color;
 }
 
+function dispatch (action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color
+      break
+    default:
+      break
+  }
+}
+
+renderApp(appState);
+dispatch({type: 'UPDATE_TITLE_TEXT', text: '《  React.js 小书  》'});
+dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'});
 renderApp(appState);
